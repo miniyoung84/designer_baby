23b1ae93f790deac35c5363951b7c915037d992b
Chase Choi <choi.minhyung@gmail.com>
2023-11-26 16:20:18 -0600

    i4: First database migration

--- src/bakery/models.py
+++ src/bakery/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class DiscordUser(models.Model):
+    booster = models.BooleanField(default=False)
+    discord_id = models.IntegerField()
