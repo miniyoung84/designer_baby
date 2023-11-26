c8371e507c9140fcaa11257f635bf49aaa13d0c7
Chase Choi <choi.minhyung@gmail.com>
2023-11-26 16:10:12 -0600

    i4: apps registered

--- src/designer_baby/settings.py
+++ src/designer_baby/settings.py
@@ -37,12 +37,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bakery.apps.BakeryConfig',
+    'cheese.apps.CheeseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ramen.apps.RamenConfig'
 ]
 
 MIDDLEWARE = [
