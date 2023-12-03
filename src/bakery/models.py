2340076b1169203d45ba6d8140a301d69f271d3c
Chase Choi <choi.minhyung@gmail.com>
2023-12-03 16:12:11 -0600

    i4: intro sound table

--- src/bakery/models.py
+++ src/bakery/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class DiscordUser(models.Model):
     booster = models.BooleanField(default=False)
     discord_id = models.IntegerField()
+
+class IntroSound(models.Model):
+    file_name = models.CharField()
+    desc = models.TextField()
+    short = models.BooleanField(default=False)
+    user = models.ForeignKey(DiscordUser, on_delete=models.CASCADE)
+    generic = models.BooleanField(default=False)
