9d53a4a39014eb3cb7429b9e25e56de16e3aafb8
Chase Choi <choi.minhyung@gmail.com>
2023-12-17 14:29:34 -0600

    i4: new table

--- src/bakery/models.py
+++ src/bakery/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class DiscordUser(models.Model):
-    booster = models.BooleanField(default=False)
     discord_id = models.IntegerField()
+
+class DiscordChannel(models.Model):
+    discord_id = models.IntegerField()
+    competitive = models.BooleanField(default=False)
 
 class IntroSound(models.Model):
     file_name = models.CharField()
