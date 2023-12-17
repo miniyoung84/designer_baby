1485ba811eab44b67b396e268aa73bb83198cf9a
Chase Choi <choi.minhyung@gmail.com>
2023-12-17 14:36:41 -0600

    i4: Edits

--- src/bakery/models.py
+++ src/bakery/models.py
@@ -8,10 +8,10 @@ class DiscordUser(models.Model):
 class DiscordChannel(models.Model):
     discord_id = models.IntegerField()
     competitive = models.BooleanField(default=False)
+    max_length = models.IntegerField()
 
 class IntroSound(models.Model):
     file_name = models.CharField()
-    desc = models.TextField()
     short = models.BooleanField(default=False)
     user = models.ForeignKey(DiscordUser, on_delete=models.CASCADE)
     generic = models.BooleanField(default=False)
