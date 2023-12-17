1d1bec1d3c9b3249b3bf30534960169b7ed17a00
Nick <nickeick@comcast.net>
2023-12-17 16:52:42 -0600

    Update status command to add arg

--- src/cogs/status_cog.py
+++ src/cogs/status_cog.py
@@ -2,13 +2,13 @@ from discord.ext import commands
 from discord import app_commands
 from discord.ext.commands import Context
 from discord import Interaction
-from discord.Client import change_presence
+from discord import Client
 from discord import Game
 from discord import Status
 
 IS_ENABLED = True
 
-class CustomCog(commands.Cog):
+class StatusCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
@@ -18,11 +18,10 @@ class CustomCog(commands.Cog):
         return IS_ENABLED
 
     @app_commands.command()
-    async def status(self, ctx: Interaction):
-        newStatus = ctx.message.content.replace('/status ', '')
-        game = Game(newStatus)
-        await selt.bot.change_presence(status=Status.idle, activity=game)
-        await ctx.response.send_message('Status updated to: ' + newStatus)
+    async def status(self, ctx, status: str):
+        game = Game(status)
+        await self.bot.change_presence(status=Status.idle, activity=game)
+        await ctx.response.send_message('Status updated to: ' + status)
 
 async def setup(bot):
-    await bot.add_cog(CustomCog(bot))
+    await bot.add_cog(StatusCog(bot))
