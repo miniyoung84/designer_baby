633db35269480733e1c4a1487eb6463ad1af7eb6
Nick <nickeick@comcast.net>
2024-05-27 22:08:39 -0500

    Fixed output to letter grade

--- src/cogs/report_card_cog.py
+++ src/cogs/report_card_cog.py
@@ -6,35 +6,35 @@ from discord.ui import View
 IS_ENABLED = True
 
 class ReportCardCog(commands.Cog):
-    grades = {
-        0: 'F',
-        1: 'D-',
-        2: 'D',
-        3: 'D+',
-        4: 'C-',
-        5: 'C',
-        6: 'C+',
-        7: 'B-',
-        8: 'B',
-        9: 'B+',
-        10: 'A-',
-        11: 'A',
-        12: 'A+'
-    }
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.grades = {
+            0: 'F',
+            1: 'D-',
+            2: 'D',
+            3: 'D+',
+            4: 'C-',
+            5: 'C',
+            6: 'C+',
+            7: 'B-',
+            8: 'B',
+            9: 'B+',
+            10: 'A-',
+            11: 'A',
+            12: 'A+'
+        }
 
     async def cog_check(self, ctx: Context):
         if (not IS_ENABLED):
             await ctx.send('This Cog is disabled')
         return IS_ENABLED
 
-    async def build_embed(self, character: str, subjects: list, grades: list) -> Embed:
+    async def build_embed(self, character: str, subjects: list, grades_value: list) -> Embed:
         description = ""
         grade_iter = 0
         for subject in subjects:
-            grade = grades[grade_iter]
-            description += (subject + ": " + str(grade) + "\n")
+            grade = self.grades[grades_value[grade_iter]]
+            description += (subject + ": " + grade + "\n")
             grade_iter += 1
         return Embed(title="Report Card", description=description).set_author(name=character)
 
@@ -48,7 +48,7 @@ class ReportCardCog(commands.Cog):
             WHERE rc.first_name LIKE %s OR rc.last_name LIKE %s OR rc.nick_name LIKE %s;
         """, (character, character, character))
         rows = await self.bot.cursor.fetchall()
-        embed = await self.build_embed(character=character, subjects=[row[2] for row in rows], grades=[row[3] for row in rows])
+        embed = await self.build_embed(character=character, subjects=[row[2] for row in rows], grades_value=[row[3] for row in rows])
         await ctx.response.send_message(embed=embed)
 
 async def setup(bot):
