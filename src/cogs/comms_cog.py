b3b1d1bd39e14220daf7be2f3f38077cba7f7173
Quinn Baker <choi.minhyung@gmail.com>
2024-05-29 03:28:53 -0500

    i18: Ping and Action system with PyNaCL install

--- src/cogs/comms_cog.py
+++ src/cogs/comms_cog.py
@@ -15,23 +15,55 @@ class CommsCog(commands.Cog):
     
     @Cog.listener("on_message")
     async def ping(self, message):
+
         if message.author == self.bot.user:
             return
-        print("message", message.content)
+        pid = '<@&1240595913261711391>'
+        myid = '<@&662827894560784419>'
+
         channel_id = message.channel.id
-        print("hi")
         await self.bot.cursor.execute("""SELECT dc.id 
         FROM bakery_discordchannel dc 
         JOIN ramen_player rp on dc.id = rp.channel_id
         WHERE dc.discord_id = %s;
         """, (channel_id,))
-        print("oof")
-        exists = await self.bot.cursor.fetchone()
-        print("exists", exists)
-        pings = ["p", "ping"]
-        if message.content.lower() in pings and exists:
-            await message.channel.send('Hello again')
-        await self.client.process_commands(message)
+
+        in_ic_channel = await self.bot.cursor.fetchone()
+        pings = ["p", "ping", ";p"]
+        mod_ping = message.content.lower() in pings
+        player_ping = message.content.lower() in ["aa", ";a"]
+        is_ping = mod_ping or player_ping
+        if is_ping and not in_ic_channel:
+            await message.channel.send("Ice Cream Channels Only!")
+            return
+        if is_ping and in_ic_channel:
+
+            async for msg in message.channel.history(limit=5):   # Cleanup on leftover pings
+                if myid in msg.content or msg.content.lower() in ["ping", "p"]:
+                    await msg.delete()
+            
+            if mod_ping:
+                await self.bot.cursor.execute("""SELECT rc.first_name, rc.last_name, rc.nick_name 
+                FROM bakery_discorduser du
+                JOIN ramen_player rp on du.id = rp.discord_user_id
+                JOIN ramen_character rc on rc.player_id = rp.id                              
+                WHERE du.discord_id = %s;
+                """, (message.author.id,))
+                name = await self.bot.cursor.fetchone()
+                if not name:
+                    await message.channel.send(f"[Please Wait for {myid}...]")
+                    return
+                name = f"{name[0]} {name[1]}" if name[0] else name[1]
+                await message.channel.send(f"[Turn Ended by {name} | {myid}]")
+                return 
+            elif player_ping:
+                await message.channel.send(f"[Awaiting Player... | {pid}]")
+        if not is_ping and in_ic_channel:
+            async for msg in message.channel.history(limit=5):   # Cleanup on leftover pings
+                if myid in msg.content or pid in msg.content or msg.content.lower() in ["ping", "p", "aa"]:
+                    await msg.delete()
+
+        await self.bot.process_commands(message)
 
 async def setup(bot):
     await bot.add_cog(CommsCog(bot))
