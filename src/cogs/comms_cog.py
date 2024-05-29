f28b56ce417ef6e1f5128738d16a333d7b8be39c
Quinn Baker <choi.minhyung@gmail.com>
2024-05-29 03:38:53 -0500

    i18 : Major pings

--- src/cogs/comms_cog.py
+++ src/cogs/comms_cog.py
@@ -27,11 +27,11 @@ class CommsCog(commands.Cog):
         JOIN ramen_player rp on dc.id = rp.channel_id
         WHERE dc.discord_id = %s;
         """, (channel_id,))
-
+        ci_msg_content = message.content.lower()
         in_ic_channel = await self.bot.cursor.fetchone()
-        pings = ["p", "ping", ";p"]
-        mod_ping = message.content.lower() in pings
-        player_ping = message.content.lower() in ["aa", ";a"]
+        pings = ["p", "ping", ";p", "mp", "mping", ";mp"]
+        mod_ping = ci_msg_content in pings
+        player_ping = ci_msg_content in ["aa", ";a", "maa", ";ma"]
         is_ping = mod_ping or player_ping
         if is_ping and not in_ic_channel:
             await message.channel.send("Ice Cream Channels Only!")
@@ -54,13 +54,20 @@ class CommsCog(commands.Cog):
                     await message.channel.send(f"[Please Wait for {myid}...]")
                     return
                 name = f"{name[0]} {name[1]}" if name[0] else name[1]
-                await message.channel.send(f"[Turn Ended by {name} | {myid}]")
+                if "m" in ci_msg_content:
+                    await message.channel.send(f"[Turn Ended by {name}, IMPORTANT RESPONSE | {myid}]")
+                else:
+                    await message.channel.send(f"[Turn Ended by {name} | {myid}]")
                 return 
             elif player_ping:
-                await message.channel.send(f"[Awaiting Player... | {pid}]")
+                if "m" in ci_msg_content:
+                    await message.channel.send(f"[IMPORTANT TURN. Awaiting Player... | {pid}]")
+                else:
+                    await message.channel.send(f"[Awaiting Player... | {pid}]")
         if not is_ping and in_ic_channel:
             async for msg in message.channel.history(limit=5):   # Cleanup on leftover pings
-                if myid in msg.content or pid in msg.content or msg.content.lower() in ["ping", "p", "aa"]:
+                if myid in msg.content or pid in msg.content or msg.content.lower() in ["p", "ping", ";p", "mp", "mping", ";mp",
+                                                                                        "aa", ";a", "maa", ";ma"]:
                     await msg.delete()
 
         await self.bot.process_commands(message)
