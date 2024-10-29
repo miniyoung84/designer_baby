a5c9572b044948e6654923ff4cc182ba81a707cb
nickeick17 <150511154+nickeick17@users.noreply.github.com>
2024-10-29 00:28:05 -0500

    Update settings.py

--- src/designer_baby/settings.py
+++ src/designer_baby/settings.py
@@ -19,6 +19,7 @@ LOCAL_DB_USER = config("LOCAL_DB_USER")
 LOCAL_DB_PW = config("LOCAL_DB_PW")
 LOCAL_DB_NAME = config("LOCAL_DB_NAME")
 LOCAL_DB_HOST = config("LOCAL_DB_HOST")
+SECRET_KEY = config("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,9 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cuf(5rewipe7o&59k3@0+jh6*mqp^a3wh!g*b0!f-i9)5w*qxz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
