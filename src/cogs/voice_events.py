c43ae743578b97d20cd1ac28c24a3ec43a444160
Quinn Baker <choi.minhyung@gmail.com>
2024-11-26 00:35:39 -0600

    i4: Bot joining and attempting audio play

---
+++ src/cogs/voice_events.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class VoiceEvents(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.Cog.listener()
+  async def on_voice_state_update(self, member, before, after):
+    # Check if the user joined a voice channel
+    if before.channel is None and after.channel is not None:
+      if not (member.guild.voice_client is None):
+        await member.guild.voice_client.disconnect()
+      voice_channel = after.channel
+      vc = await voice_channel.connect()
+      vc.play(discord.FFmpegPCMAudio('placeholder.mp3'))
+      # Wait for the audio to finish playing
+      while vc.is_playing():
+        await discord.utils.sleep_until(vc.loop.time() + 1)
+
+async def setup(bot):
+  await bot.add_cog(VoiceEvents(bot))
